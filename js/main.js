8ea6e90592407e7f2383b311021c9a5e7ea83584
pachipaz <raquelpaz.gutierrez@gmail.com>
2016-11-02 15:06:57 -0300

    envio final bicis locas

--- js/main.js
+++ js/main.js
@@ -1,58 +1,39 @@
 function validateForm(){
-	//ingreso nombre
-	ingresoNombre=document.getElementById('name').value;
-	if( ingresoNombre == null || ingresoNombre.length == 0) {
-       alert ('Escribe tu nombre!!');
-       return false; 
-    }
-       
-    //permite caracteres de la A-Z
-	letrasNombre=document.getElementById('name').value;
-		if (/[0-9]/.test(letrasNombre)) {
-		alert ('Nombre no valido ingresa solo letras!');
+	var ingresoNombre = document.getElementById('name').value;
+	if (ingresoNombre==null||ingresoNombre.length==0 ||/[0-9]/.test(ingresoNombre)){
+		alert ('campo obligatorio ingrese su nombre');
 		return false;
 	}
-	//ingreso letra mayuscula nombre
-	mayusculaName = ingresoName.charAt(0);
-    if (!(/[A-Z]/.test(mayusculaName))){
-        alert("Ingrese primera letra con mayúscula");
+	else if (ingresoNombre.charAt(0)!=ingresoNombre.charAt(0).toUpperCase()){
+		alert('ingrese la primera letra con mayuscula');
+		return false;
+	}
+
+	var ingresoApellido=document.getElementById('lastname').value;
+	if (ingresoApellido==null||ingresoApellido.length==0 ||/[0-9]/.test(ingresoApellido)){
+		alert ('campo obligatorio ingrese su apellido');
+		return false;
+	}
+	else if (ingresoApellido.charAt(0)!=ingresoApellido.charAt(0).toUpperCase()){
+		alert('ingrese la primera letra con mayuscula');
+		return false;
+	}
+
+	var ingresoMail=document.getElementById('input-email').value;
+	if  (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(mail))){
+        alert("ingrese dirección de correo electrónico valida");
         return false;
-    }
- 	//ingreso apellido 
-	ingresoApellido=document.getElementById('lastname').value;
-    if (ingresoApellido ==null ||ingresoApellido.length ==0 ){
-	alert ('escribe tu apellido!!');
-	return false;
+   }
+	var ingresoClave=document.getElementById('input-password').value;
+	if (ingresoClave.length<6|| ingresoClave=='password'||ingresoClave==123456||ingresoClave==098754){
+
+		alert ('ingrese una contraseña valida');
+		return false; 
 	}
-	//permite caracteres de la A-Z
-	letrasApellido=document.getElementById('lastname').value;
-		if (/[0-9]/.test(letrasApellido)) {
-		alert ('nombre no valido ingresa solo letras!');
-		return false;
-	}
-	//ingreso mayuscula
-	mayusculaApellido = ingresoApellido.charAt(0);
-    if (!(/[A-Z]/.test(mayusculaApellido))){
-        alert ('ingrese primera letra con mayúscula');
-        return false;
-    }
-	//validacion email 
-	ingresoMail=document.getElementById('email').value;
-	
-	if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(correctEmail))) {
-         alert('ingreso un mail correcto!');
-         return false;
-    }
-    // clave 
-	ingresoClave=document.getElementById('password').value;
-	if (ingresoClave.length<6||ingresoClave=='password'||ingresoClave==123456||ingresoClave==0987654){
-		alert ('contraseña muy facil');
-		return false;
-	}
-	// seleccion bici 
-	ingresoBici= document.querySelector('select').value;
+
+	var ingresoBici=document.querySelector('select').value;
 	if (ingresoBici==0){
-		alert ('elige tu bici');
+		alert ('selecciona tu tipo de bici');
 		return false;
 	}
 }
